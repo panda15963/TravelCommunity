83b777a0f7a43fe9aca7c2b4e656b25c362b2cbc
Minseok Choi <seok7521@gmail.com>
2023-02-11 22:17:50 +0900

    main

--- backend/mainApp/serializers.py
+++ backend/mainApp/serializers.py
@@ -3,3 +3,8 @@ from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from rest_framework.validators import UniqueValidator
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators = [UniqueValidator(queryset=User.objects.all())],
+    )
