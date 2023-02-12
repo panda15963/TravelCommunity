4874f76cb6bb9f1cc2814b875e1a8df6bc9eb574
Minseok Choi <seok7521@gmail.com>
2023-02-12 22:04:12 +0900

    main

--- backend/mainApp/serializers.py
+++ backend/mainApp/serializers.py
@@ -8,3 +8,21 @@ class RegisterSerializer(serializers.ModelSerializer):
         required=True,
         validators = [UniqueValidator(queryset=User.objects.all())],
     )
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password],)
+    password2 = serializers.CharField(write_only=True, required=True)
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'password2')
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return data
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        token = Token.objects.create(user=user)
+        return token
