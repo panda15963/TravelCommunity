2015d29dc6eafe5c7a788ab61d814ec0e30b2b4a
Minseok Choi <seok7521@gmail.com>
2023-02-14 17:06:18 +0900

    main

--- backend/mainApp/urls.py
+++ backend/mainApp/urls.py
@@ -3,5 +3,5 @@ from .views import RegisterView, LoginView, ProfileView
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('profile/', ProfileView.as_view(), name='profile'),
+    path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
 ]
