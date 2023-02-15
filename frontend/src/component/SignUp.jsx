a312a793f5b50806518cf9db4479d75f26062ad3
Minseok Choi <seok7521@gmail.com>
2023-02-15 11:42:43 +0900

    main

--- frontend/src/component/SignUp.jsx
+++ frontend/src/component/SignUp.jsx
@@ -1,5 +1,6 @@
 import { React, Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from "axios"
 //Features for Main Pages
 import Menubar from "../component/navbar";
 import Footer from "../component/footer";
@@ -16,6 +17,37 @@ const linkStyle = {
   marginBottom: '8px',
 }
 export default class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+      password2: "",
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    const target = event.target;
+    this.setState({
+      [target.name]: target.value,
+    });
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3000/mainApp/register/", {
+        username: this.state.username,
+        password: this.state.password,
+        email: this.state.email,
+        password2: this.state.password2,
+      })
+      .then((response) => {
+        console.log(response.data);
+        this.props.history.push("/login");
+      });
+  }
   render() {
     return (
       <>
@@ -25,27 +57,27 @@ export default class SignUp extends Component {
             <div>
               <img style={imageStyle} src={`${process.env.PUBLIC_URL}/img/components/signup.png`} alt="" />
             </div>
-            <form className="form-detail" method="post" id="myform">
+            <form className="form-detail" method="post" id="myform" onSubmit={this.handleSubmit}>
               <h2>SIGN UP FORM</h2>
               <div className="form-row form-row-1">
                 <label htmlFor="user_name">Username</label>
-                <input type="text" name="user_name" id="user_name" className="input-text" />
+                <input type="text" name="user_name" id="user_name" defaultValue={this.state.username} onChange={this.handleChange} className="input-text" required />
               </div>
               <div className="form-row form-row-1">
                 <label htmlFor="your_email">Your Email</label>
-                <input type="text" name="your_email" id="your_email" className="input-text" required pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}" />
+                <input type="text" name="your_email" id="your_email" defaultValue={this.state.email} onChange={this.handleChange} className="input-text" required pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}" />
               </div>
               <div className="form-row form-row-1 ">
                 <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="password" className="input-text" required />
+                <input type="password" name="password" id="password" defaultValue={this.state.password} onChange={this.handleChange} className="input-text" required />
               </div>
               <div className="form-row form-row-1">
                 <label htmlFor="comfirm-password">Comfirm Password</label>
-                <input type="password" name="comfirm_password" id="comfirm_password" className="input-text" required />
+                <input type="password" name="comfirm_password" id="comfirm_password" defaultValue={this.state.password2} onChange={this.handleChange2} className="input-text" required />
                 <Link style={linkStyle} to="/SignIn">Have An Account?</Link>
               </div>
               <div className="form-row-last">
-                <input type="submit" name="register" className="register" value="Register" />
+                <input type="submit" name="register" className="register" defaultValue="Register" />
               </div>
             </form>
           </div>
