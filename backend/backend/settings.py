9534704b7643df502baba68bec922270b81d6d04
Minseok Choi <seok7521@gmail.com>
2023-02-15 16:35:58 +0900

    main

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'mainApp',
-    'corsheaders'
+    'corsheaders',
+    'posts'
 ]
 
 MIDDLEWARE = [
---
+++ backend/posts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from mainApp.models import Profile
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE,blank=True)
+    title = models.CharField(max_length=128)
+    category = models.CharField(max_length=128)
+    body = models.TextField()
+    image = models.ImageField(upload_to='post/', default='default.png')
+    Likes = models.ManyToManyField(User)
+    published_date = models.DateTimeField(default=timezone.now)
