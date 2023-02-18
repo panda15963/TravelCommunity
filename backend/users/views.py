fca4ceca273030f9b934e9fc7d86b203160caee9
Minseok Choi <seok7521@gmail.com>
2023-02-18 21:44:16 +0900

    main

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import RegisterSerializer
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
